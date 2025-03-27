52245a3526ed7c1e8e02e6579bca08e060bc49b2
Erandika30 <erandikarathnayake@gmail.com>
2025-03-27 17:31:45 +0530

    fixed the error of fetching

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -25,7 +25,7 @@ export default function Register() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/user/register", {
+      const response = await fetch("http://localhost:3000/user/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
