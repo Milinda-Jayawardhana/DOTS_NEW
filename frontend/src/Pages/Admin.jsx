fe20f2bf397dbb1b65d340b684528f4a38980d92
Erandika30 <erandikarathnayake30@gmail.com>
2025-05-16 20:41:18 +0530

    update admin

--- frontend/src/Pages/Admin.jsx
+++ frontend/src/Pages/Admin.jsx
@@ -3,16 +3,28 @@ import AddColorForm from '../Components/AddColorForm'
 import AddSizeForm from '../Components/AddSizeForm'
 import AddMaterialForm from '../Components/AddMaterialForm'
 import AddCountForm from '../Components/AddCountForm'
+import AddTypeForm from '../Components/AddTypeForm'
+import GetAllContacts from '../Components/GetAllContacts'
+import Footer from '../Components/Footer'
 
 export default function Admin() {
   return (
     <div>
-      <div className='flex gap-10 pt-10'>
-      <AddColorForm></AddColorForm>
-      <AddSizeForm></AddSizeForm>
-      <AddMaterialForm></AddMaterialForm>
-      <AddCountForm></AddCountForm>
+      <div className='flex gap-10 pt-10 justify-evenly'>
+        <AddColorForm></AddColorForm>
+        <AddSizeForm></AddSizeForm>
+        <AddMaterialForm></AddMaterialForm>
+        <AddCountForm></AddCountForm>
+        
+
       </div>
+      <div className='flex gap-10 pt-10 justify-evenly'>
+        <AddTypeForm></AddTypeForm>
+      </div>
+      <div>
+        <GetAllContacts></GetAllContacts>
+      </div>
+      <Footer></Footer>
     </div>
   )
 }
